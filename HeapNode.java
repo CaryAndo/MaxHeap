e0ed7e1e363e50acb2d8c14fda4f189da205c89a
CaryAndo <caryando@gmail.com>
2014-05-02 01:34:17 -0700

    writing heapify

--- HeapNode.java
+++ HeapNode.java
@@ -1,45 +1,61 @@
-public class HeapNode {
-    private int[] children;
-    private int root = 1;
-    private int current = 1; // Point at root
+public class BinaryHeap {
+    private int[] array;
+    private int size = 101;
+    private int root = 0;
 
-    public HeapNode(int[] passed) {
-        children = new int[2*passed.length];
+    public BinaryHeap(int[] passed) {
+        array = new int[2*passed.length];
 
         for (int i = 0; i < passed.length; i++)
-            children[i] = passed[i];
+            array[i] = passed[i];
+    }
+
+    public BinaryHeap(int root) {
+        array = new int[size];
+        array[1] = root;
+        for (int i = 2; i < size; i++)
+            array[i] = null;
+    }
+
+    private void swap(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
     private void heapify() {
-        // TODO
-    } 
-
-    public int getLeft(int position) {
-        return children[2 * position];
-    }
-
-    public int getLeft() {
-        return children[2 * current];
-    }
-
-    public int getRight(int position) {
-        return children[2 * position + 1];
-    }
-
-    public int getRight() {
-        return children[2 * current + 1];
-    }
-
-
-// FML
-    public void addNode(int value) {
-        int point = root;
-        if (children[root] != null)
+        for (int k = 0; k < array.length/2; k++)
         {
-            if (getLeft)
+            if (array[k] < array[k*2] && array[k] < array[(k*2)+1])
+            {
+                // TODO
+            }
+            else if (array[k] < array[k*2] || array[k] < array[(k*2)+1])
+            {
+                if (array[k] < array[k*2])
+                {
+                    swap(k, k*2);
+                    continue;
+                }
+                else if (array[k])
+
+            }
         }
     }
 
+    public void addNode(int value) {
+        for (int j = 1; j < array.length; j++)
+        {
+            if (array[j] == null)
+            {
+                array[j] = value;
+                break;
+            }
+            else 
+                continue;
+        }
+        heapify();
+    }
 
 
 }
