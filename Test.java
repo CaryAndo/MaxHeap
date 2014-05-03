246ea24a206fdc1620b2d1f2cf36463a76271343
CaryAndo <caryando@gmail.com>
2014-05-02 22:00:40 -0700

    class finished

--- Test.java
+++ Test.java
@@ -2,19 +2,7 @@ public class Test {
     public static void main(String[] args) {
         int[] a = new int[5];
         BinaryHeap heap = new BinaryHeap(12);
-        heap.addNode(13);
-        heap.addNode(16);
-        heap.addNode(25);
-        heap.addNode(1);
-        heap.addNode(9);
-        heap.addNode(4);
-        heap.addNode(2);
-        heap.addNode(6);
-        heap.addNode(200);
-        heap.addNode(65);
-        heap.addNode(8);
-        heap.addNode(92);
-        heap.addNode(19);
+        for (int i = )
         System.out.println("Original: ");
         heap.print();
         heap.removeRoot();
